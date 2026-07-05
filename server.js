519fef4d36c06aa307ccaac302c5df17d1a184cf
medicenhoy <emartinezcabello@byupathway.edu>
2026-07-05 00:41:03 -0300

    002

--- server.js
+++ server.js
@@ -11,11 +11,14 @@ const port = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Configuración de las vistas
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// Configuración de la carpeta pública
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Rutas de tu aplicación
 app.get('/', (req, res) => {
     res.render('index', { title: 'Home' });
 });
@@ -32,6 +35,7 @@ app.get('/categories', (req, res) => {
     res.render('categories', { title: 'Service Project Categories' });
 });
 
+// Arranque del servidor
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
